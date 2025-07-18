5d4450c2fe41892bd27accf32a686f872ed7f565
Scott Tatham <scottytatham@gmail.com>
2025-07-18 23:38:17 +1000

    Clearing docs
    - Clearing the docs folder which will trigger a rebuild with the correct write settings for the Workflow. It didn't trigger on the non change in the browser after last commit.

--- docs/js/window.js
+++
@@ -1,9 +0,0 @@
-/**
- * Determine if the window is in landscape mode.
- * @returns {boolean} Returns whether the window is in landscape mode.
- * @constructor
- */
-function IsHorizontal()
-{
-    return window.innerWidth > window.innerHeight;
-}

ac067f5dcead6852ad937ea89734ad8d438d8d7c
Scott Tatham <scottytatham@gmail.com>
2025-07-18 20:12:30 +1000

    Initial Commit
    + Adding the entire project in a relatively complete state.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+namespace PortfolioWebsite
+{
+    using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    
+    /// <summary>
+    /// The entry point of the application.
+    /// </summary>
+    public class Program
+    {
+        #region Initialisation
+        
+        public static async Task Main(string[] args)
+        {
+            WebAssemblyHostBuilder webAssemblyHostBuilder = WebAssemblyHostBuilder.CreateDefault(args);
+            webAssemblyHostBuilder.RootComponents.Add<App>("#app");
+            webAssemblyHostBuilder.RootComponents.Add<HeadOutlet>("head::after");
+
+            webAssemblyHostBuilder.Services.AddScoped(_ => new HttpClient
+            {
+                BaseAddress = new Uri(webAssemblyHostBuilder.HostEnvironment.BaseAddress)
+            });
+
+            await webAssemblyHostBuilder.Build().RunAsync();
+        }
+        
+        #endregion
+    }
+}
